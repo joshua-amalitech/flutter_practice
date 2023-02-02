705c26e08161aed0e5331eeaf2bfb8f2b71f4fbd
Joshua Tetteh <joshua.tetteh@amalitech.com>
2023-02-02 15:27:43 +0000

    Setup Imports and all needed files

---
+++ lib/imports.dart
@@ -0,0 +1,17 @@
+export 'package:flutter/material.dart';
+
+
+
+
+export 'utilities/utilities.dart';
+
+export 'models/models.dart';
+
+export 'services/services.dart';
+
+export 'controllers/controllers.dart';
+
+export 'widgets/widgets.dart';
+
+export 'views/views.dart';
+
